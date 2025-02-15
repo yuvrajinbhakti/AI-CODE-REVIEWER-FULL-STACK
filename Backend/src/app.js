642a5f0e70cf5aab474d948d9a54e7df0fb4be0f
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-02-15 12:05:25 +0530

    Initial Commit

---
+++ Backend/src/app.js
@@ -0,0 +1,7 @@
+require('dotenv').config(); 
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+module.exports=app;
---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,9 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+const prompt = "Explain how AI works";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text()); 
